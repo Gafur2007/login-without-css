fd7d14bea75047d504da50b77cf38d831684be5c
Gafur2007 <tohirovabdugofur@gmail.com>
2024-03-19 15:31:33 +0500

    lorem

--- src/App.js
+++ src/App.js
@@ -16,22 +16,22 @@ function App() {
           <p className="text-base text-black opacity-80 font-normal mb-px max-xl:text-white">
             Welcome back
           </p>
-          <h1 className="text-3xl font-bold mb-6 max-xl:text-white max-sm:mb-[0px]">
+          <h1 className="text-3xl font-bold mb-6 max-xl:text-white max-sm:mb-[0px] mr-[10px] font-[16px]">
             Login to your account
           </h1>
-          <p className="text-black opacity-80 font-normal mb-3 max-xl:text-white max-sm:mb-[0px]">
+          <p className="text-black opacity-80 font-normal mb-3 max-xl:text-white max-sm:mb-[0px] mt-[0px]">
             Email
           </p>
           <input
-            className="rounded-md box-border border-2 pl-[20px] pt-4 pb-4 w-[347px]  placeholder:text-slate-400 block bg-white max-sm:mb-[0px] w-[300px] pt-2 pb-2"
+            className="rounded-md box-border border-2 pl-[20px] pt-4 pb-4 w-[347px]  placeholder:text-slate-400 block bg-white max-sm:mb-[0px] w-[290px] pt-1 pb-1"
             type="email"
             placeholder="John.snow@gmail.com"
           />
-          <p className="text-black opacity-80 font-normal mb-3 mt-3 max-xl:text-white max-sm:mb-[0px]">
+          <p className="text-black opacity-80 font-normal mb-3 mt-3 max-xl:text-white max-sm:mb-[0px] mt-[0px]">
             Password
           </p>
           <input
-            className="rounded-md box-border border-2 pl-[20px] pt-4 pb-4 w-[347px] placeholder:text-slate-400 block bg-white max-sm:mb-[0px] w-[300px] pt-2 pb-2"
+            className="rounded-md box-border border-2 pl-[20px] pt-4 pb-4 w-[347px] placeholder:text-slate-400 block bg-white max-sm:mb-[0px] w-[290px] pt-1 pb-1"
             type="password"
             placeholder="********"
           />
@@ -47,13 +47,13 @@ function App() {
           </ul>
 
           <button
-            className="bg-green-500 block w-[347px] rounded-md pt-5 pb-5 font-bold text-white text-base hover:text-black mb-3.5 w-[300px] pt-2 pb-2"
+            className="bg-green-500 block w-[347px] rounded-md pt-5 pb-5 font-bold text-white text-base hover:text-black mb-3.5 w-[290px] pt-1.5 pb-1.5"
             type="submit"
           >
             Login now
           </button>
           <button
-            className="mb-[142px] justify-center bg-slate-800 flex items-center block w-[347px] rounded-md pt-5 pb-5 font-bold text-white text-base   max-xl:mb-[50px]  max-sm:mb-[10px] w-[300px] pt-2 pb-2"
+            className="mb-[142px] justify-center bg-slate-800 flex items-center block w-[347px] rounded-md pt-5 pb-5 font-bold text-white text-base   max-xl:mb-[50px]  max-sm:mb-[10px] w-[290px] pt-1.5 pb-1.5"
             type="submit"
           >
             <img
